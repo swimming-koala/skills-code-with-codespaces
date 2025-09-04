ec78014932a1d387e78c301956105642809dd57f
swimming-koala <70906949+swimming-koala@users.noreply.github.com>
2025-09-04 09:56:29 +0000

    feat: Changed back to the original postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install sl -F
+sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
